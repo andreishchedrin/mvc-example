36b04bdc98fb0f684fef6811671cd9045fdb67f9
andreishchedrin <schedrin-post@rambler.ru>
2021-01-16 15:43:25 +0300

    autowire refactor

--- core/Container.php
+++ core/Container.php
@@ -2,11 +2,9 @@
 
 namespace app\core;
 
+use app\core\errors\container\InfiniteRecursionException;
 use app\core\errors\container\NotFoundContainerException;
-use Closure;
-use Exception;
 use Psr\Container\ContainerInterface;
-use ReflectionClass;
 
 /**
  * Class Container
@@ -17,6 +15,8 @@ use ReflectionClass;
 class Container implements ContainerInterface
 {
     private $providers;
+    private $values = [];
+    private $calls = [];
 
     public function __construct(ProviderInterface ...$providers)
     {
@@ -25,12 +25,21 @@ class Container implements ContainerInterface
 
     public function get($id)
     {
-        $provider = $this->findProvider($id);
-        if (null === $provider) {
-            throw new NotFoundContainerException($id);
+        if (in_array($id, $this->calls)) {
+            throw new InfiniteRecursionException($id, $this->calls);
+        }
+        $this->calls[] = $id;
+        if (!array_key_exists($id, $this->values)) {
+            $provider = $this->findProvider($id);
+            if (null === $provider) {
+                throw new NotFoundContainerException($id);
+            }
+            $this->values[$id] = $provider->provide($id, $this);
         }
 
-        return $provider->provide($id, $this);
+        array_pop($this->calls);
+
+        return $this->values[$id];
     }
 
     public function has($id)
